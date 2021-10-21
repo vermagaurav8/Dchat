d4e51ec1bd27a12fd6e627f9739ccded6da4a846
Guarav Verma <gaurav.verma382001@gmail.com>
2021-10-21 12:05:02 +0530

    created Header

---
+++ src/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { username, user } from "./user";
+
+  function signout() {
+    user.leave();
+    username.set("");
+  }
+</script>
+
+<header>
+  <h1>🔫💬</h1>
+  {#if $username}
+    <div class="user-bio">
+      <span> Hello <strong>{$username}</strong></span>
+      <img
+        src={`https://avatars.dicebear.com/api/initials/${$username}.svg`}
+        alt="avatar"
+      />
+    </div>
+
+    <button class="signout-button"> Sign Out </button>
+  {:else}
+    <h3>DCHAT App</h3>
+  {/if}
+</header>
