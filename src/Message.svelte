dd8a857dc8a344b4c5c41f6cb5de29b885d973d7
Guarav Verma <gaurav.verma382001@gmail.com>
2021-10-21 12:41:24 +0530

    Added ChatMessage

---
+++ src/Message.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let message;
+  export let sender;
+
+  const messageClass = message.who === sender ? "sent" : "received";
+  const avatar = `https://avatars.dicebear.com/api/initials/${message.who}.svg`;
+  const timeStamp = new Date(message.when);
+</script>
+
+<div class={`message ${messageClass}`}>
+  <img src={avatar} alt="Avatar" />
+  <div class="message-text">
+    <p>{message.what}</p>
+    <time>{timeStamp.toLocaleTimeString()}</time>
+  </div>
+</div>
